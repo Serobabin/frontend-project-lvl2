14aa4e76bf0d02f5046d3050ffcdd0094db2815b
Danil Serobabin <serobabin.danil@gmail.com>
2021-09-16 12:59:01 +0300

    Add engine

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
+import genDiff from '../src/index.js';
 
 const program = new Command();
 
@@ -8,6 +9,9 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2, type) => {
+     console.log(genDiff(filepath1, filepath2));
+  });
 
 program.parse();
