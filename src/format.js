164ee1aaca624d5566cba4a5095ed57adbc9f558
Danil Serobabin <serobabin.danil@gmail.com>
2021-12-12 16:40:34 +0300

    fix support nested structures

--- src/format.js
+++ src/format.js
@@ -1,4 +1,11 @@
 import stylish from './formatters/stylish.js';
 
-const format = (tree, type) => stylish(tree);
+const getFormatterType = (type) => type.format;
+const format = (tree, type) => {
+  const formatterType = getFormatterType(type);
+  if (formatterType === 'stylish') {
+    return stylish(tree);
+  }
+  return stylish(tree);
+};
 export default format;
